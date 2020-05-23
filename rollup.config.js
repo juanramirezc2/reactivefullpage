c2730314fa60bdccb6eb335d5d033cef6bc039d5
Juan Ramirez <tuxgabo@gmail.com>
2020-05-23 18:21:27 -0500

    the error persist

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
+
 
 export default {
   input: 'src/main.js',
@@ -8,6 +10,9 @@ export default {
     format: 'cjs'
   },
   plugins: [resolve(),
+    commonjs({
+      include: 'node_modules', // Default: undefined
+    }),
     babel({
       presets: ["@babel/preset-env","@babel/preset-react"],
       plugins: [
