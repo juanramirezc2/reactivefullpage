ad3e5c10bf3c1a7876837ac21db38ee655e6b655
Juan Ramirez <tuxgabo@gmail.com>
2020-05-24 12:07:56 -0500

    new version now es modules is supported

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,28 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
-
+import pkg from './package.json'
 
 export default {
   input: 'src/main.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'cjs'
-  },
-  plugins: [resolve(),
-    commonjs({
-      include: 'node_modules', // Default: undefined
-    }),
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+    }],
+  plugins: [
     babel({
       presets: ["@babel/preset-env","@babel/preset-react"],
-      plugins: [
-      ],
       exclude: 'node_modules/**',
-    })
-  ]
+    }),
+    resolve(),
+    commonjs()
+  ],
+  external: ['react']
 };
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import "index.css"
 
-export default ({children})=>{
+export default reactive
+const reactive = ({children})=>{
   return (
     <div>
       {children.map(child=><div className="fullpage-item">{child}</div>)}
